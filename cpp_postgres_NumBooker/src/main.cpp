73c7eaab73172e801ef8756223f869798b9ff390
iPlugin <ipluginua@gmail.com>
2024-10-21 21:37:33 +0300

    project №2

---
+++ cpp_postgres_NumBooker/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+
+#include "numbooker.cpp"
+#include "libraries/inih/INIReader.h"
+
+const char* getUserName(char* envp[]) {
+    const char* userVar = "USER=";
+    for (int i = 0; envp[i] != nullptr; ++i) {
+        if (std::strncmp(envp[i], userVar, std::strlen(userVar)) == 0) {
+            return envp[i] + std::strlen(userVar);
+        }
+    }
+    return nullptr;
+}
+
+DatabaseConfig getDataINI(const std::string& filename) {
+    INIReader reader(filename);
+
+    DatabaseConfig data;
+    data.host = reader.GetString("DataBase", "host", "unknown");
+    data.port = reader.GetInteger("DataBase", "port", 0000);
+    data.username = reader.GetString("DataBase", "username", "unknown");
+    data.password = reader.GetString("DataBase", "password", "unknown");
+
+    return data;
+}
+
+
+int main(int argc, char* argv[], char* envp[]) {
+
+    const char* userName = getUserName(envp);
+    DatabaseConfig dbConfig = getDataINI("settings/config.ini");
+
+    NumBooker book(userName, dbConfig);
+    book.run();
+    
+    return 0;
+}
---
+++ cpp_postgres_NumBooker/src/numbooker.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+
+struct DatabaseConfig {
+    std::string host;
+    int port;
+    std::string username;
+    std::string password;
+};
+
+class NumBooker {
+private:
+    std::string userName;
+    DatabaseConfig db;
+
+public:
+    NumBooker(const char* name, DatabaseConfig dbConfig);
+    ~NumBooker();
+    bool run();
+    void welcome();
+    char option();
+    void branching(const char choice);
+
+    void optionShow();
+    void optionAdd();
+    void optionDel();
+    void optionEdit();
+    void optionSearch();
+    
+    void bye();
+};
+
+NumBooker::NumBooker(const char* name, DatabaseConfig dbConfig)
+    : userName(name ? name : ""), db(dbConfig) {
+
+}
+
+NumBooker::~NumBooker() {}
+
+bool NumBooker::run() {
+    welcome();
+    
+    while (true) {
+        char choice = option();
+        if (choice == 'q') {break;}
+        branching(choice);
+    }
+
+    bye();
+    
+    return true;
+}
+
+
+void NumBooker::welcome() {
+    std::cout << R"(
+    Starting the NumBooker application...
+
+        Hello)" << " " + userName << R"(! We are excited to introduce our new program
+    a phonebook created using C++ and PostgreSQL.
+    This program allows you to conveniently store and manage contacts.
+    )" << std::endl;
+}
+
+char NumBooker::option() {
+    char result;
+    std::cout << R"(
+        Choose an option [ 0 .. 5 or q]
+    [1] Show all contacts
+    [2] Add a contact
+    [3] Delete the contact
+    [4] Edit the contact
+    [5] Contact search
+    [q] Exit the program
+    
+     :)";
+    std::cin >> result;
+    return result;
+}
+
+void NumBooker::branching(const char choice) {
+    switch (choice) {
+    case '1':
+        optionShow();
+        break;
+    case '2':
+        optionAdd();
+        break;
+    case '3':
+        optionDel();
+        break;
+    case '4':
+        optionEdit();
+        break;
+    case '5':
+        optionSearch();
+        break;
+    
+    default:
+        std::cout << R"(
+        Incorrect option selected. Format: number or q
+        )" << std::endl;
+        break;
+    }
+}
+
+void NumBooker::optionShow() {
+    std::cout << "optionShow" << std::endl;
+}
+void NumBooker::optionAdd() {
+    std::cout << "optionAdd" << std::endl;
+}
+void NumBooker::optionDel() {
+    std::cout << "optionDel" << std::endl;
+}
+void NumBooker::optionEdit() {
+    std::cout << "optionEdit" << std::endl;
+}
+void NumBooker::optionSearch() {
+    std::cout << "optionSearch" << std::endl;
+}
+
+void NumBooker::bye() {
+    std::cout << R"(
+        Bye )" << userName + "!" << R"(
+    
+    Closing the NumBooker application...
+    )" << std::endl;
+}
