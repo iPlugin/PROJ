b1c493014f54ca48ab03afd650590ef1fbfcbf9f
iPlugin <ipluginua@gmail.com>
2024-11-12 01:06:50 +0200

    11.11.2024

--- cpp_QML_Qt_BlackJack/BlackJack/build/Desktop_Qt_6_8_0-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appBlackJack_FILE /home/iplugin/Codes/github/PROJ/cpp_QML_Qt_BlackJack/BlackJack/build/Desktop_Qt_6_8_0-Debug/appBlackJack)
-set(__QT_DEPLOY_TARGET_appBlackJack_TYPE EXECUTABLE)
